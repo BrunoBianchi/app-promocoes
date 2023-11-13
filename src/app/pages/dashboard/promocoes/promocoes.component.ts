51339e9f9a2749e4a44792d54ddf2e18c809e9ee
Bruno Bianchi <bruno.raiadobian@gmail.com>
2023-11-13 13:02:54 -0300

    Add Promo form + alerts + connection to db

--- src/app/pages/dashboard/promocoes/promocoes.component.ts
+++ src/app/pages/dashboard/promocoes/promocoes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-promocoes',
@@ -6,6 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./promocoes.component.scss'],
 })
 export class PromocoesComponent implements OnInit {
-  ngOnInit(): void {}
-  constructor() {}
+  public errorMsg!: string;
+  public successMsg!: string;
+  public promoList!: any[];
+  public addForm: FormGroup = this.formbuilder.group({
+    name: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    option: ['', [Validators.required]],
+    date: ['', [Validators.required]],
+    url: ['', [Validators.required]],
+  });
+  ngOnInit(): void {
+
+  }
+  public search(value: string) {}
+  constructor(
+    private formbuilder: FormBuilder,
+    private firestore: AngularFirestore
+  ) {}
+  public addPromo() {
+    this.firestore
+      .collection('promocoes')
+      .add(this.addForm.value)
+      .then((res) => {
+        this.successMsg = `Promoção ${this.addForm.value['name']} adicionada com sucesso!`;
+      })
+      .catch((err) => {
+        this.errorMsg = err.message;
+      });
+  }
 }
